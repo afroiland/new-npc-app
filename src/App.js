724dc736074ac188e2e3142cecab5be6d0365ca8
Andrew Froiland <andrew.froiland@gmail.com>
2018-12-15 16:38:02 -0600

    Add navbar tabs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
-//import logo from "./logo.svg";
 import "./App.css";
+import NavTabs from "./components/tabs";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <p>test</p>
+        <NavTabs />
       </div>
     );
   }
