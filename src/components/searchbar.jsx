043d1a7df8c898fb68f810b5f3170ef2758d67dc
Andrew Froiland <andrew.froiland@gmail.com>
2018-12-18 13:11:43 -0600

    Add folder for functions

--- src/components/searchbar.jsx
+++ src/components/searchbar.jsx
@@ -2,14 +2,10 @@ import React, { Component } from "react";
 import { FormGroup, FormControl, Button, Navbar } from "react-bootstrap";
 
 class SearchBar extends Component {
-  state = {};
   render() {
     return (
       <Navbar>
         <Navbar.Header>
-          <Navbar.Brand>
-            <a href="#home">Brand</a>
-          </Navbar.Brand>
           <Navbar.Toggle />
         </Navbar.Header>
         <Navbar.Collapse>
@@ -17,7 +13,6 @@ class SearchBar extends Component {
             <FormGroup>
               <FormControl type="text" placeholder="Search" />
             </FormGroup>{" "}
-            <Button type="submit">Submit</Button>
           </Navbar.Form>
         </Navbar.Collapse>
       </Navbar>
