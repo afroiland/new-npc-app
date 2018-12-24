1563cc1f76f7e379ab63dd5a5d04565fb53ef746
afroiland <andrew.froiland@gmail.com>
2018-12-24 12:12:40 -0600

    Formatting

--- src/components/generateContent.jsx
+++ src/components/generateContent.jsx
@@ -16,24 +16,28 @@ class GenerateContent extends Component {
   handleGenerate = (level, pcClass) => {
     let newNPC = generate(level, pcClass);
     console.log("newNPC: ", newNPC);
-    this.setState({npc: newNPC});
+    this.setState({ npc: newNPC });
   }
 
   render() {
     return (
       <div>
-        <FormGroup controlId="formControlsSelect">
-          <FormControl componentClass="select" placeholder="select" onChange={e => this.setState({pcClass: e.target.value})}>
-            <option value="fighter">fighter</option>
-            <option value="thief">thief</option>
-          </FormControl>
-          <FormControl componentClass="select" placeholder="select" onChange={e => this.setState({level: e.target.value})}>
-            {levelRange.map(level => <option key={level} value={level}>{level}</option>)}
-          </FormControl>
-        </FormGroup>
-        <Button onClick={() => this.handleGenerate(this.state.level, this.state.pcClass)}>Generate</Button>
-        <Button>Save</Button>
         <Grid>
+          <Row>
+            <Col md={6}>
+              <FormControl componentClass="select" placeholder="select" onChange={e => this.setState({ pcClass: e.target.value })}>
+                <option value="fighter">Fighter</option>
+                <option value="thief">Thief</option>
+              </FormControl>
+            </Col>
+            <Col md={6}>
+              <FormControl componentClass="select" placeholder="select" onChange={e => this.setState({ level: e.target.value })}>
+                {levelRange.map(level => <option key={level} value={level}>{level}</option>)}
+              </FormControl>
+            </Col>
+          </Row>
+          <Button onClick={() => this.handleGenerate(this.state.level, this.state.pcClass)}>Generate</Button>
+          <Button>Save</Button>
           <Row>
             <Col md={12}>
               <Column name="New NPC" />
