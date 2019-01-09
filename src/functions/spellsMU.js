87ea3e95e07d933d2899b3c4d7a5e8984e40b75c
afroiland <andrew.froiland@gmail.com>
2019-01-09 15:26:57 -0600

    Add arrays of 1st and 2nd level MU spells

--- src/functions/spellsMU.js
+++ src/functions/spellsMU.js
@@ -1,5 +1,9 @@
-const level_1_spells = [];
-const level_2_spells = [];
+const level_1_spells = ["Burning Hands", "Charm Person", "Comprehend Languages", "Dancing Lights", "Detect Magic", "Enlarge",
+    "Feather Fall", "Find Familiar", "Hold Portal", "Identify", "Light", "Magic Missile", "Message", "Nystul's Magc Aura",
+    "Protection from Evil", "Push", "Shield", "Shocking Grasp", "Sleep", "Spider Climb", "Unseen Servant"];
+const level_2_spells = ["Continual Light", "Darkness 15' Radius", "Detect Evil", "Detect Invisibility", "ESP", "Forget",
+    "Invisibility", "Knock", "Leomund's Trap", "Levitate", "Magic Mouth", "Mirror Image", "Pyrotechnics", "Ray of Enfeeblement",
+    "Rope Trick", "Scare", "Shatter", "Stinking Cloud", "Strength", "Web", "Wizard Lock"];
 const level_3_spells = [];
 const level_4_spells = [];
 const level_5_spells = [];
