33a679104debbf34899ac957bda22054ecd1f58a
afroiland <andrew.froiland@gmail.com>
2019-01-17 16:03:46 -0600

    Create config folder

--- server/server.js
+++ server/server.js
@@ -1,6 +1,21 @@
 const express = require('express');
 const mysql = require('mysql');
 
+const db = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'new_schema'
+});
+
+db.connect((err) => {
+  if(err) {
+    console.log("err: ", err);
+    throw err;
+  }
+  console.log("db connected");
+});
+
 const app = express();
 
 app.set("port", process.env.PORT || 3001);
