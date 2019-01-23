1add4a67e7ac8bf6fe5182400abb35272b1cbc42
afroiland <andrew.froiland@gmail.com>
2019-01-23 11:56:09 -0600

    Updaate NPCDetails to use props

--- src/components/NPCList.jsx
+++ src/components/NPCList.jsx
@@ -2,12 +2,12 @@ import React, { Component } from "react";
 
 class NPCList extends Component {
   render() {
-    console.log("NPCList props: ", this.props);
+    //console.log("NPCList props: ", this.props);
     return (
-      // <button onClick={(e) => this.props.handleClick(e)}>ok</button>
+      // <button onClick={(e) => this.props.handleNameClick(e)}>ok</button>
 
       <div>
-        <ul>{this.props.list.map(npc => <li key={npc.name} onClick={() => this.props.handleClick(npc.name)}>{npc.name}</li>)}</ul>
+        <ul>{this.props.list.map(npc => <li key={npc.name} onClick={() => this.props.handleNameClick(npc.name)}>{npc.name}</li>)}</ul>
       </div>
     );
   }
--- src/components/viewContent.jsx
+++ src/components/viewContent.jsx
@@ -2,36 +2,41 @@ import React, { Component } from "react";
 import NPCList from "./NPCList";
 import NPCDetails from "./NPCDetails";
 import { generate } from "./../functions/generate"
-import { Col, Grid, Row } from "react-bootstrap";
+import { Button, Col, FormControl, Grid, Row } from "react-bootstrap";
 import axios from "axios";
 
-var npc1 = {
-  id: 1,
-  name: "Test Name",
-  title: "",
-  level: 0,
-  class: "",
-  currentHP: 0,
-  maxHP: 0,
-  ac: 0,
-  str: 0,
-  ex_str: 0,
-  int: 0,
-  dex: 0,
-  con: 0,
-  wis: 0,
-  cha: 0,
-  spells: {},
-  items: "",
-  ranking: 0,
-  affiliation: "",
-  notes: ""
-};
+const levelRange = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const classes = ["Fighter", "Magic-User", "Cleric", "Thief", "Monk", "Assassin"]
+
+// var npc1 = {
+//   id: 1,
+//   name: "Test Name",
+//   title: "",
+//   level: 0,
+//   class: "",
+//   currentHP: 0,
+//   maxHP: 0,
+//   ac: 0,
+//   str: 0,
+//   ex_str: 0,
+//   int: 0,
+//   dex: 0,
+//   con: 0,
+//   wis: 0,
+//   cha: 0,
+//   spells: {},
+//   items: "",
+//   ranking: 0,
+//   affiliation: "",
+//   notes: ""
+// };
 
 class ViewContent extends Component {
   state = {
-    NPCList: [],
-    selectedNPC: npc1,
+    levelSelect: 1,
+    classSelect: "Fighter",
+    NPCList: [{name: "Test Name"}],
+    // selectedNPC: npc1,
     name: "",
     title: "",
     level: 0,
@@ -57,9 +62,7 @@ class ViewContent extends Component {
     items: "",
     probity: 0,
     affiliation: "",
-    notes: "",
-    levelSelect: 1,
-    pcClass: 'Fighter'
+    notes: ""
   };
 
   render() {
@@ -69,11 +72,54 @@ class ViewContent extends Component {
         <Grid>
           <Row>
             <Col sm={2}>
-              <NPCList list={this.state.NPCList} handleClick={this.handleClick} />
+              <NPCList list={this.state.NPCList} handleNameClick={this.handleNameClick} />
             </Col>
-            <Col sm={6}>
-              <NPCDetails npc={this.state.selectedNPC} handleChange={this.handleChange}
-                handleGenerate={this.handleGenerate} handleSave={this.handleSave} handleClear={this.handleClear}/>
+            <Col sm={10}>
+              <Row>
+                <Col md={6}>
+                  <FormControl componentClass="select" placeholder="select" onChange={e => this.setState({ classSelect: e.target.value })}>
+                    {classes.map(pcClass => <option key={pcClass} value={pcClass}>{pcClass}</option>)}
+                  </FormControl>
+                </Col>
+                <Col md={6}>
+                  <FormControl componentClass="select" placeholder="select" onChange={e => this.setState({ levelSelect: parseInt(e.target.value) })}>
+                    {levelRange.map(level => <option key={level} value={level}>{level}</option>)}
+                  </FormControl>
+                </Col>
+              </Row>
+              <Button onClick={() => this.handleGenerate(this.state.levelSelect, this.state.classSelect)}>Generate</Button>
+              <Button onClick={() => this.handleSave(this.state)}>Save</Button>
+              <Button onClick={() => this.handleClear()}>Clear</Button>
+
+              <NPCDetails handleChange={this.handleChange}
+                handleGenerate={this.handleGenerate} handleSave={this.handleSave} handleClear={this.handleClear}
+                name={this.state.name}
+                title={this.state.title}
+                level={this.state.level}
+                class={this.state.class}
+                race={this.state.race}
+                currentHP={this.state.currentHP}
+                maxHP={this.state.maxHP}
+                ac={this.state.ac}
+                str={this.state.str}
+                ex_str={this.state.ex_str}
+                int={this.state.int}
+                dex={this.state.dex}
+                con={this.state.con}
+                wis={this.state.wis}
+                cha={this.state.cha}
+                spellbookLvl_1={this.state.spellbookLvl_1}
+                spellbookLvl_2={this.state.spellbookLvl_2}
+                spellbookLvl_3={this.state.spellbookLvl_3}
+                spellbookLvl_4={this.state.spellbookLvl_4}
+                spellbookLvl_5={this.state.spellbookLvl_5}
+                memorized={this.state.memorized}
+                items={this.state.items}
+                probity={this.state.probity}
+                affiliation={this.state.affiliation}
+                notes={this.state.notes}
+                
+                />
             </Col>
           </Row>
         </Grid>
@@ -89,13 +135,40 @@ class ViewContent extends Component {
     });
   }
 
-  handleClick = (name) => {
+  handleNameClick = (name) => {
     console.log("name: ", name);
     let selectedNPC = this.state.NPCList.filter(obj => {
       return obj.name === name
     });
     console.log("selectedNPC: ", selectedNPC);
-    this.setState({ selectedNPC: selectedNPC });
+    this.setState({
+      name: selectedNPC.name,
+      level: selectedNPC.level,
+      title: selectedNPC.title,
+      class: selectedNPC.class,
+      race: selectedNPC.race,
+      currentHP: selectedNPC.currentHP,
+      maxHP: selectedNPC.maxHP,
+      ac: selectedNPC.ac,
+      str: selectedNPC.str,
+      ex_str: selectedNPC.ex_str,
+      int: selectedNPC.int,
+      dex: selectedNPC.dex,
+      con: selectedNPC.con,
+      wis: selectedNPC.wis,
+      cha: selectedNPC.cha,
+      spellbookLvl_1: selectedNPC.spellbookLvl_1,
+      spellbookLvl_2: selectedNPC.spellbookLvl_2,
+      spellbookLvl_3: selectedNPC.spellbookLvl_3,
+      spellbookLvl_4: selectedNPC.spellbookLvl_4,
+      spellbookLvl_5: selectedNPC.spellbookLvl_5,
+      memorized: selectedNPC.memorized,
+      gold: selectedNPC.gold,
+      items: selectedNPC.items,
+      probity: selectedNPC.probity,
+      affiliation: selectedNPC.affiliation,
+      notes: selectedNPC.notes
+    });
   }
 
   handleGenerate = (level, pcClass) => {
@@ -134,9 +207,9 @@ class ViewContent extends Component {
   handleSave = (state) => {
     console.log("handleSave state: ", state);
     axios.post("http://localhost:3001/add", state)
-    .then(res => {
-      console.log("add res: ", res);
-    });
+      .then(res => {
+        console.log("add res: ", res);
+      });
   }
 
   handleChange = (e) => {
