140d5355a84c75ee952d6af5de4ed0623c658c1d
afroiland <andrew.froiland@gmail.com>
2019-01-29 19:03:01 -0600

    Add calcThac0 function

--- src/functions/combat.js
+++ src/functions/combat.js
@@ -8,11 +8,11 @@ export function fight(groupA, groupB) {
 
   // establish vars for each NPC
   let cgrpA = groupA.map(npc => ({"name": npc.name, "ac": npc.ac, "hp": npc.currentHP, "thac0": npc.thac0,
-    "damage": getDamage(npc.weapon), "spells": npc.memorized, "incap": false}));
+    "damage": getDamage(npc.weapon), "dmgBonus": getDmgBonus(npc.str, npc.ex_str), "spells": npc.memorized, "incap": false}));
   console.log("cgrpA: ", cgrpA);
 
   let cgrpB = groupB.map(npc => ({"name": npc.name, "ac": npc.ac, "hp": npc.currentHP, "thac0": npc.thac0,
-    "damage": getDamage(npc.weapon), "spells": npc.memorized, "incap": false}));
+    "damage": getDamage(npc.weapon), "dmgBonus": getDmgBonus(npc.str, npc.ex_str), "spells": npc.memorized, "incap": false}));
   console.log("cgrpB: ", cgrpB);
 
   // initiative
@@ -42,6 +42,9 @@ export function fight(groupA, groupB) {
 }
 
 function getDamage(weapon) {
-  console.log("weapon: ", weapon);
   return "1-8";
+}
+
+function getDmgBonus(str, ex_str) {
+  return 1;
 }
