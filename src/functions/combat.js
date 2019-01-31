7d8fc2172deb2bb51d24ad3e28a6b936bf9133fb
afroiland <andrew.froiland@gmail.com>
2019-01-30 19:55:49 -0600

    Work on fight function

--- src/functions/combat.js
+++ src/functions/combat.js
@@ -1,12 +1,8 @@
 import { rollDice } from "./dice";
 
 export function fight(incomingGroupA, incomingGroupB) {
-  console.log("incomingGroupA: ", incomingGroupA);
-  console.log("incomingGroupB: ", incomingGroupB);
-
   let log = [];
 
-  // establish vars for each NPC
   let groupA = incomingGroupA.map(npc => ({"name": npc.name, "ac": npc.ac, "hp": npc.currentHP, "thac0": npc.thac0,
     "damage": getDamage(npc.weapon), "dmgBonus": getDmgBonus(npc.str, npc.ex_str), "spells": npc.memorized, "incap": false}));
   console.log("groupA: ", groupA);
@@ -15,13 +11,49 @@ export function fight(incomingGroupA, incomingGroupB) {
     "damage": getDamage(npc.weapon), "dmgBonus": getDmgBonus(npc.str, npc.ex_str), "spells": npc.memorized, "incap": false}));
   console.log("groupB: ", groupB);
 
-  // initiative
-  let groupAInit = rollDice(1, 6);
-  let groupBInit = rollDice(1, 6);
-  console.log("inits: ", groupAInit, groupBInit);
+  doOneRound();
 
-  // side one each NPC does an action
-  // check if NPC is incap
+  function doOneRound() {
+    let groupAInit = rollDice(1, 6);
+    let groupBInit = rollDice(1, 6);
+    console.log("inits: ", groupAInit, groupBInit);
+
+    if (groupAInit > groupBInit) {
+      oneSideGoes(groupA, groupB);
+      oneSideGoes(groupB, groupA);
+    } else if (groupAInit < groupBInit) {
+      oneSideGoes(groupB, groupA);
+      oneSideGoes(groupA, groupB);
+    } else if (groupAInit === groupBInit) {
+      console.log("simultaneous");
+    }
+  }
+
+  function oneSideGoes(attackingGroup, defendingGroup) {
+    // loop through each npc
+    for (let i = 0; i < attackingGroup.length; i++) {
+      if (attackingGroup[i].incap) {
+        break;
+      }
+      // select target
+      let target = {};
+      while (Object.keys(target).length === 0) {
+        let tempTarget = defendingGroup[rollDice(1, defendingGroup.length) -1];
+        console.log("tempTarget: ", tempTarget);
+        if (tempTarget.incap) {
+          tempTarget = {};
+        } else {
+          target = tempTarget;
+        }
+      }
+      console.log("target: ", target);
+      // attack
+
+      // dmg
+
+      // change incap status of target if necessary
+    }
+  }
 
 
   // side two each NPC does an action
