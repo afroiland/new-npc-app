ad412de26fe8a2eb6a0923b2801d4bc8579ed791
afroiland <andrew.froiland@gmail.com>
2019-02-09 16:29:32 -0600

    Start working on material nav tabs

--- src/components/tabs.jsx
+++ src/components/tabs.jsx
@@ -1,27 +1,69 @@
 import React, { Component } from "react";
-import { Tabs, Tab, TabList, TabPanel } from "react-tabs";
+// import { Tabs, Tab, TabList, TabPanel } from "react-tabs";
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import { Link, Route, BrowserRouter, Switch } from "react-router-dom";
 import ViewContent from "./viewContent";
 import Combat from "./combat";
+// import ReactCSSTransitionGroup from "react-addons-css-transition-group";
 
 class NavTabs extends Component {
+  state = {
+    value: 0
+  };
+
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
+  handleChangeIndex = index => {
+    this.setState({ value: index });
+  };
+
   render() {
     return (
-      <div>
-        <Tabs defaultIndex={0}>
-          <TabList>
-            <Tab>NPCs</Tab>
-            <Tab>Combat</Tab>
-          </TabList>
-          <TabPanel>
-            <ViewContent />
-          </TabPanel>
-          <TabPanel>
-              <Combat />
-          </TabPanel>
-        </Tabs>
-      </div>
+      <BrowserRouter>
+        <div>
+          <AppBar>
+            <Tabs
+              value={this.state.value}
+              onChange={this.handleChange}
+              indicatorColor="primary"
+              textColor="primary"
+              centered
+            >
+              <Tab label="NPCs" component={Link} to="/npcs" />
+              <Tab label="Combat" component={Link} to="/combat" />
+            </Tabs>
+          </AppBar>
+          <Switch>
+            {/* <Route path="/npcs" component={PageShell(ItemOne)} />
+          <Route path="/combat" component={PageShell(ItemTwo)} /> */}
+            <Route path="/npcs" component={ViewContent} />
+            <Route path="/combat" component={Combat} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
+
+// const PageShell = (Page, previous) => {
+//   return props => (
+//     <div className="page">
+//       <ReactCSSTransitionGroup
+//         transitionAppear={true}
+//         transitionAppearTimeout={600}
+//         transitionEnterTimeout={600}
+//         transitionLeaveTimeout={600}
+//         transitionName={props.match.path === "/one" ? "SlideIn" : "SlideOut"}
+//       >
+//         {console.log(props)}
+//         <Page {...props} />
+//       </ReactCSSTransitionGroup>
+//     </div>
+//   );
+// };
 
 export default NavTabs;
