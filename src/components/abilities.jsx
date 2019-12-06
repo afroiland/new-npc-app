ae14611d1401620e4411bca9b8374ada4db8e649
afroiland <andrew.froiland@gmail.com>
2019-12-05 19:19:02 -0600

    Refactor abilities component using arrow functions

--- src/components/abilities.jsx
+++ src/components/abilities.jsx
@@ -1,33 +1,29 @@
-import React, { Component } from "react";
+import React from "react";
 import { TextField } from "@material-ui/core";
 
-class Abilities extends Component {
-  render() {
-    const { abilities } = this.props;
-    return (
-      <TextField
-        label='Class Abilities'
-        value={formatAbilities(abilities)}
-        margin="normal"
-        variant="outlined"
-        multiline
-        style={{ width: '100%', marginTop: '7px' }}
-      />
-    );
-  }
-}
-
-function formatAbilities(abilitiesArray) {
-  let result = "";
-
-  for (let i = 0; i < abilitiesArray.length; i++) {
-    result += abilitiesArray[i];
-    if (i !== abilitiesArray.length - 1) {
-      result += "\n";
+const Abilities = abilities => {
+  
+  const formatAbilities = (abilitiesArray) => {
+    let result = "";
+    for (let i = 0; i < abilitiesArray.length; i++) {
+      result += abilitiesArray[i];
+      if (i !== abilitiesArray.length - 1) {
+        result += "\n";
+      }
     }
+    return result;
   }
 
-  return result;
-}
+  return (
+    <TextField
+      label='Class Abilities'
+      value={formatAbilities(abilities.abilities)}
+      margin="normal"
+      variant="outlined"
+      multiline
+      style={{ width: '100%', marginTop: '7px' }}
+    />
+  );
+};
 
 export default Abilities;
