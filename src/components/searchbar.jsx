dc7c66e887e554797e3b4411647ddab50a5e2a98
afroiland <andrew.froiland@gmail.com>
2019-12-06 20:35:51 -0600

    Update npm, remove return stmt from searchBar component

--- src/components/searchbar.jsx
+++ src/components/searchbar.jsx
@@ -3,21 +3,19 @@ import InputBase from '@material-ui/core/InputBase';
 import SearchIcon from '@material-ui/icons/Search';
 
 const SearchBar = (classes, handleSearchChange) => {
-  return (
-    <div className={classes.search}>
-      <div className={classes.searchIcon}>
-        <SearchIcon />
-      </div>
-      <InputBase
-        placeholder="Search…"
-        classes={{
-          root: classes.inputRoot,
-          input: classes.inputInput,
-        }}
-        onChange={e => handleSearchChange(e.target.value)}
-      />
+  <div className={classes.search}>
+    <div className={classes.searchIcon}>
+      <SearchIcon />
     </div>
-  );
+    <InputBase
+      placeholder="Search…"
+      classes={{
+        root: classes.inputRoot,
+        input: classes.inputInput,
+      }}
+      onChange={e => handleSearchChange(e.target.value)}
+    />
+  </div>
 };
 
 export default SearchBar;
